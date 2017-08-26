9fcb70d9eea3f7fc611b775173e06c94742a1af4
Richard Maya <richard.maya94@gmail.com>
2017-08-26 14:35:29 -0500

    Search Bar Running!

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -12,7 +12,7 @@ export class HeaderComponent {
                 private router:Router){}
 
   buscar_producto(termino:string){
-      console.log(termino);
+    //   console.log(termino);
       this.router.navigate(['buscar', termino]);
   }
 
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -5,10 +5,41 @@ import { Http } from "@angular/http";
 export class ProductosService {
 
   productos: any[] = [];
+  producto_filtrado: any[] = [];
   cargando: boolean = true;
 
   constructor(private http: Http) {
     this.cargar_productos();
+  }
+
+  public buscar_producto ( termino:string ){
+      console.log("Buscando producto");
+      console.log(this.productos.length);
+
+      if(this.productos.length === 0){
+          this.cargar_productos().then(()=>{
+              // terminó la carga
+              this.filtrar_productos(termino);
+          });
+      }else{
+          this.filtrar_productos(termino);
+      }
+
+
+  }
+
+  private filtrar_productos(termino:string){
+      this.producto_filtrado = [];
+      termino = termino.toLowerCase();
+
+      this.productos.forEach(prod=>{
+
+          if( prod.categoria.indexOf(termino)>=0 || prod.titulo.toLowerCase().indexOf(termino)>=0 ){
+              this.producto_filtrado.push(prod);
+          }
+
+        //   console.log(prod);
+      })
   }
 
   public cargar_producto( id:string ){
@@ -17,12 +48,17 @@ export class ProductosService {
 
   public cargar_productos() {
 
-    this.http.get('https://paginamamalona.firebaseio.com/productos_idx.json')
-      .subscribe(res => {
-        //   console.log(res.json());
-        this.cargando = false;
-        this.productos = res.json();
-      })
+      let promesa = new Promise((resolve, reject) =>{
+          this.http.get('https://paginamamalona.firebaseio.com/productos_idx.json')
+            .subscribe(res => {
+              //   console.log(res.json());
+              this.cargando = false;
+              this.productos = res.json();
+              resolve();
+            })
+      });
+
+      return promesa;
 
   }
 
