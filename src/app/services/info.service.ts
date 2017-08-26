8a738852b524dccbe40b630d7f97bdc8462fff87
Richard Maya <richard.maya94@gmail.com>
2017-08-26 02:11:13 -0500

    Database via Firebase

--- src/app/services/info.service.ts
+++ src/app/services/info.service.ts
@@ -5,12 +5,26 @@ import { Http } from "@angular/http";
 export class InfoService {
 
   info:any = {};
+  equipo:any[] = [];
 
   constructor( public http:Http ) {
+    this.carga_info();
+    this.carga_about();
+  }
+
+  public carga_info(){
     this.http.get("assets/data/info.pagina.json")
               .subscribe ( data =>{
-                  console.log(data.json());
+                  // console.log(data.json());
                   this.info = data.json();
+              })
+  }
+
+  public carga_about(){
+    this.http.get("https://paginamamalona.firebaseio.com/team.json")
+              .subscribe ( data =>{
+                  console.log(data.json());
+                  this.equipo = data.json();
               })
   }
 
